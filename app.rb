960c36fee6ec6cdd6441a0bbe7343d2e81dd90af
sirbotta <hipersimon@gmail.com>
2012-10-12 17:58:44 +0200

    menù dinamico

--- app.rb
+++ app.rb
@@ -1,14 +1,17 @@
 require 'sinatra'
 require 'haml'
 
+
+##nav = {"index" => true,"about" => false, "cv" => false }
+
 get '/' do
-  haml :index
+  haml :index, :locals => {:active => "index"}
 end
 
 get '/about' do
-  haml :about
+  haml :about, :locals => {:active => "about"}
 end
 
 get '/cv' do
-  haml :cv
+  haml :cv, :locals => {:active => "cv"}
 end
